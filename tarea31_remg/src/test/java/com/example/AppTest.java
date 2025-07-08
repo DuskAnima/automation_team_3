c990ddd31d593d5fa0ca22f289300a84bb65a6fc
ROSA ESTER M. <82519357+ROSAESTERMG@users.noreply.github.com>
2025-07-08 11:13:09 -0400

    tarea3_1
    avances tarea3_1

---
+++ tarea31_remg/src/test/java/com/example/AppTest.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import com.example.model.Tarea;
+
+public class AppTest {
+    private AppTarea manager;
+
+     @BeforeEach
+    void setUp() {
+        manager = new AppTarea();
+    }
+
+
+    //----------agregar tarea--------//
+    @Test
+    void crearTarea(){
+        Tarea  inv =  manager.crearTarea("Los Dinosaurios","Trabajo de Investigación del Dr. Jaime Castillo");
+        assertThat(inv.getId(),isEqualTo(1));
+        assertThat(manager.getAll()).hasSize(1);
+
+    }
+
+    //------------actualizar Tarea ------------//
+    @Test
+    void actualizarTarea() {
+        Tarea inv  = manager.crearTarea("Los Dinosaurios", "Trabajo de Investigación del Dr. Jaime Castillo");
+        boolean updated = manager.updateTitulo(inv.getId(), "Los Dinosaurios 2025");
+        assertThat(updated).isTrue();
+        assertThat(inv.getTitulo()).isEqualTo("Los Dinosaurios 2025");
+    }
+
+    //------------eliminar tarea ------------------//
+     @Test
+    void eliminarTarea() {
+        Tarea inv  = manager.crearTarea("Los Dinosaurios", "Trabajo de Investigación del Dr. Jaime Castillo");
+        boolean removed = manager.deleteTarea(inv.getId());
+        assertThat(removed).isTrue();
+        assertThat(manager.getAll()).isEmpty();
+  }
+
+
+
+}
