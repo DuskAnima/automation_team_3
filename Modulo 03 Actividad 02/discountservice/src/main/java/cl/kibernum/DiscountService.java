9f01fafaa9ab50af27f6f7158951ccf3c4872d4b
Ricardo <ricardo.ponce.galaz@gmail.com>
2025-07-08 18:07:48 -0400

    Actividades Ricardo

---
+++ Modulo 03 Actividad 02/discountservice/src/main/java/cl/kibernum/DiscountService.java
@@ -0,0 +1,18 @@
+package cl.kibernum;
+
+/**
+ * Hello world!
+ *
+ */
+public class DiscountService 
+{
+    public double calcularDescuento(double monto) {
+        if (monto > 100000) {
+            return monto * 0.85; 
+        } else if (monto >= 50000) {
+            return monto * 0.90; 
+        } else {
+            return monto; 
+        }
+    }
+}
