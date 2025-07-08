302c426729ef88192caef774e1921d057dbfe135
ROSA ESTER M. <82519357+ROSAESTERMG@users.noreply.github.com>
2025-07-08 11:25:53 -0400

    creando Trea.java

--- tarea31_remg/src/main/java/com/example/Tarea.java
+++ tarea31_remg/src/main/java/com/example/Tarea.java
@@ -1,5 +1,18 @@
 package com.example;
 
 public class Tarea {
+  private final int id;
+  private final String titulo;
+  private final String descripcion;
     
+    public Tarea(int id, String titulo, String descripcion  ){
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion =descripcion;
+    
+    }
+
+
+
+
 }
