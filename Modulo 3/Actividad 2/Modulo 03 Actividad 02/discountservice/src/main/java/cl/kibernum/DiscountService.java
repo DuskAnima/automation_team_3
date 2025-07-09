2688370e61887c6dabfdae909859cc845540e005
Anima <cjaccardh@gmail.com>
2025-07-09 17:06:09 -0400

    Cambios en tests de actividad 2

--- Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/main/java/cl/kibernum/DiscountService.java
+++ Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/main/java/cl/kibernum/DiscountService.java
@@ -7,20 +7,14 @@ package cl.kibernum;
 public class DiscountService 
 {
     public double calcularDescuento(double monto) {
-        try {
-            if (monto <= 0) {
-                throw new IllegalArgumentException("El monto no puede ser negativo")
-            }
-        } catch (IllegalArgumentException e) {
-            // TODO: handle exception
-        } {
-            ;
-        } else if (monto > 100000) {
-            return monto * 0.85; 
-        } else if (monto >= 50000) {
-            return monto * 0.90; 
+        if (monto <= 0) { // Numero negativo error
+            throw new IllegalArgumentException("El monto no puede ser negativo");
+        } else if (monto > 100000) { // Si monto mayor a 100k
+            return monto * 0.85; // 15% de descuento (retorna 85%)
+        } else if (monto >= 50000) { // Si monto mayor a 50k
+            return monto * 0.90; // 10% de desciento (retorna 90%)
         } else {
-            return monto; 
+            return monto; // Monto menor 49.999, precio completo
         }
     }
 }
--- Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/test/java/cl/kibernum/DiscountServiceTest.java
+++ Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/test/java/cl/kibernum/DiscountServiceTest.java
@@ -9,30 +9,31 @@ public class DiscountServiceTest
 {
      DiscountService discountService = new DiscountService();
 
-    @Test
-    public void testDescuento15Porciento() {
-        double monto = ThreadLocalRandom.current().nextDouble(100000.1, 900000);
-        monto = Math.round(monto * 10.0) / 10.0;
-        double montoConDescuento = discountService.calcularDescuento(monto);
-        montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
-        assertEquals((monto * 0.85), montoConDescuento, 900000);
-    }
 
     @Test
-    public void testNoDescuentoBajo50000() {
-        double monto = ThreadLocalRandom.current().nextDouble(1, 49999.9);
-        monto = Math.round(monto * 10.0) / 10.0;
-        double montoConDescuento = discountService.calcularDescuento(monto);
-        montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
-        assertEquals(monto, montoConDescuento, 0);
+    public void testDescuento15Porciento() { // Prueba mayor a 100,000.1
+        // Monto aleatorio dentro de los parámetros
+        double monto = ThreadLocalRandom.current().nextDouble(100000.1, 900000); 
+        double montoEsperado = monto * 0.85; // Se declara monto esperado
+        double montoConDescuento = discountService.calcularDescuento(monto); // Cálculo del servicio
+        assertEquals(montoEsperado, montoConDescuento, 0.000001); // Margen de error de 6 decimales
     }
-
+    
     @Test
-    public void testDescuento10Porciento() {
+    public void testDescuento10Porciento() { // Prueba entre 50k y 100k
         double monto = ThreadLocalRandom.current().nextDouble(50000, 100000);
         monto = Math.round(monto * 10.0) / 10.0;
         double montoConDescuento = discountService.calcularDescuento(monto);
         montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
         assertEquals((monto * 0.9), montoConDescuento, 100000);
+    }
+
+    @Test
+    public void testSinDescuento() {
+        double monto = ThreadLocalRandom.current().nextDouble(1, 49999.9);
+        monto = Math.round(monto * 10.0) / 10.0;
+        double montoConDescuento = discountService.calcularDescuento(monto);
+        montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
+        assertEquals(monto, montoConDescuento, 0);
     }
 }
