aad3381b693fcb0f433293a7ff8dc2b1a84ef2f0
Anima <cjaccardh@gmail.com>
2025-07-09 22:47:22 -0400

    Corrección de tests

--- Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/test/java/cl/kibernum/DiscountServiceTest.java
+++ Modulo 3/Actividad 2/Modulo 03 Actividad 02/discountservice/src/test/java/cl/kibernum/DiscountServiceTest.java
@@ -11,29 +11,25 @@ public class DiscountServiceTest
 
 
     @Test
-    public void testDescuento15Porciento() { // Prueba mayor a 100,000.1
-        // Monto aleatorio dentro de los parámetros
-        double monto = ThreadLocalRandom.current().nextDouble(100000.1, 900000); 
-        double montoEsperado = monto * 0.85; // Se declara monto esperado
-        double montoConDescuento = discountService.calcularDescuento(monto); // Cálculo del servicio
-        assertEquals(montoEsperado, montoConDescuento, 0.000001); // Margen de error de 6 decimales
+    public void shouldDiscount_15Percent() { // Prueba mayor a 100,000.1
+        double inputAmount = ThreadLocalRandom.current().nextDouble(100000.1, 900000); // Número aleatorio
+        double discountApplied = discountService.calcularDescuento(inputAmount); // Cálculo del servicio
+        // Se compara el monto esperado (inputMonto * 0.85) con el resultado procesado por el servicio. 
+        assertEquals((inputAmount * 0.85), discountApplied, 0.000001); // Margen de error de 6 decimales
     }
     
     @Test
-    public void testDescuento10Porciento() { // Prueba entre 50k y 100k
-        double monto = ThreadLocalRandom.current().nextDouble(50000, 100000);
-        monto = Math.round(monto * 10.0) / 10.0;
-        double montoConDescuento = discountService.calcularDescuento(monto);
-        montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
-        assertEquals((monto * 0.9), montoConDescuento, 100000);
+    public void shouldDiscount_10Percent() { // Prueba entre 50k y 100k
+        double inputAmount = ThreadLocalRandom.current().nextDouble(50000, 100000);
+        double discountApplied = discountService.calcularDescuento(inputAmount);
+        // Se compara el monto esperado (inputMonto * 0.9) con el resultado procesado por el servicio. 
+        assertEquals((inputAmount * 0.9), discountApplied, 0.000001);
     }
 
     @Test
-    public void testSinDescuento() {
+    public void noDiscountShouldApply() {
         double monto = ThreadLocalRandom.current().nextDouble(1, 49999.9);
-        monto = Math.round(monto * 10.0) / 10.0;
         double montoConDescuento = discountService.calcularDescuento(monto);
-        montoConDescuento = Math.round(montoConDescuento * 10.0) / 10.0;
-        assertEquals(monto, montoConDescuento, 0);
+        assertEquals(monto, montoConDescuento, 0); // Sin operaciones no hay necesidad de un márgen de error.
     }
 }
