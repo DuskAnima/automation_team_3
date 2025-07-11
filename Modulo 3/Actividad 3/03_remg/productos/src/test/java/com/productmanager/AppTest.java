83e73b6640ca787e8e58eff95ad42ada2b37927b
ROSA ESTER M. <82519357+ROSAESTERMG@users.noreply.github.com>
2025-07-10 20:34:55 -0400

    comienzo

--- Modulo 3/Actividad 3/03_remg/productos/src/test/java/com/productmanager/AppTest.java
+++ Modulo 3/Actividad 3/03_remg/productos/src/test/java/com/productmanager/AppTest.java
@@ -1,15 +1,17 @@
 package com.productmanager;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
  
 public class ProductManagerTest 
 {
-    
-    @Test
-    public void shouldAnswerWithTrue()
+    ProductManager productManager;
     {
-        assertTrue( true );
+        @beforeEach();
+        void setup()
+        {
+            productManager = new ProductManager();        }
     }
+
 }
