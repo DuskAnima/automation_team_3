afad90c94da100aea0ccc8a99c7835c60a13b08d
ROSA ESTER M. <82519357+ROSAESTERMG@users.noreply.github.com>
2025-07-10 20:43:00 -0400

    --etapa1

--- Modulo 3/Actividad 3/03_remg/productos/src/test/java/com/productmanager/AppTest.java
+++ Modulo 3/Actividad 3/03_remg/productos/src/test/java/com/productmanager/AppTest.java
@@ -14,4 +14,14 @@ public class ProductManagerTest
             productManager = new ProductManager();        }
     }
 
+    @Test
+    void testAddProduct()
+    {   String  nombre ='Comida para gatos etapa 1';
+        String  descripcion = "Comida semi humeda para felinos etapa 1 de 3 hasta 6 meses.";
+        int     precio = 12990;
+
+
+
+    }
+
 }
