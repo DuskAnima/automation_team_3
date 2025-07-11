e5add99366dbf190fa68eba5ab2151dbc1b0d736
Anima <cjaccardh@gmail.com>
2025-07-10 21:57:27 -0400

    test stuff

--- Modulo 3/Actividad 3/Actividad 3 template/productmanager/src/test/java/cl/kibernumacademy/models/ProductManagerTest.java
+++ Modulo 3/Actividad 3/Actividad 3 template/productmanager/src/test/java/cl/kibernumacademy/models/ProductManagerTest.java
@@ -2,6 +2,7 @@ package cl.kibernumacademy.models;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import static org.hamcrest.MatcherAssert.*;
 
 public class ProductManagerTest {
     private ProductManager manager; // servicio
@@ -12,20 +13,25 @@ public class ProductManagerTest {
     }
 
 // TODO: Hacer tests para CRUD
-// Implementar pruebas assert con JUnit y Hamcrest. Usar assumeTrue() o assumeThat(). Integrar @ParameterizedTest, @BeforeEach y @AfterEach
-
+// Implementar pruebas assert con JUnit y Hamcrest. Usar assumeTrue() o assumeThat(). Integrar @ParameterizedTest, @BeforeEach y @AfterEach  
+    @DisplayName("Prueba de agregación de productos")
     @Test 
     shouldAddProduct() {
-
+        // Cada producto debe tener nombre, descripción y precio.
+        // Debe almacenarse en una lista
     }
 
+    @DisplayName("Prueba de modificación de productos")
     @Test
     shouldUpdateProduct() {
-
+        // Permitir modificar nombre, descripción o precio
+        // Validación de que el producto existe
     }
 
+    @DisplayName("Prueba de eliminación de productos")
     @Test
     shouldDeleteProduct() {
-
+        // Eliminar producto desdde su Id
+        // Validar que el producto existe
     }
 }
