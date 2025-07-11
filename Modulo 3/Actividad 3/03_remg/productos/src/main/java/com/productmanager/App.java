5973dc7672ff24d7a2cc11955248d467081d3553
ROSA ESTER M. <82519357+ROSAESTERMG@users.noreply.github.com>
2025-07-10 20:25:51 -0400

    desde cero

---
+++ Modulo 3/Actividad 3/03_remg/productos/src/main/java/com/productmanager/App.java
@@ -0,0 +1,13 @@
+package com.productmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
