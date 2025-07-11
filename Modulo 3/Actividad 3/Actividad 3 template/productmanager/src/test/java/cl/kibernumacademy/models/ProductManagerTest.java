d312b5523b942cd734777d320a5a50cac4016c50
Anima <cjaccardh@gmail.com>
2025-07-10 20:02:31 -0400

    creando tests

---
+++ Modulo 3/Actividad 3/Actividad 3 template/productmanager/src/test/java/cl/kibernumacademy/models/ProductManagerTest.java
@@ -0,0 +1,31 @@
+package cl.kibernumacademy.models;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+
+public class ProductManagerTest {
+    private ProductManager manager; // servicio
+
+    @BeforeEach
+    void setUp() {
+        manager = new ProductManager();
+    }
+
+// TODO: Hacer tests para CRUD
+// Implementar pruebas assert con JUnit y Hamcrest. Usar assumeTrue() o assumeThat(). Integrar @ParameterizedTest, @BeforeEach y @AfterEach
+
+    @Test 
+    shouldAddProduct() {
+
+    }
+
+    @Test
+    shouldUpdateProduct() {
+
+    }
+
+    @Test
+    shouldDeleteProduct() {
+
+    }
+}
