425f4e241d87d7c874fce84b31c8db39fb78fd10
Anima <cjaccardh@gmail.com>
2025-07-15 04:13:47 -0400

    doin' green

--- Modulo 3/Actividad 4/Modulo03-Actividad04-Equipo03/tasktracker/src/test/java/cl/kibernumacademy/models/TaskTrackerTest.java
+++ Modulo 3/Actividad 4/Modulo03-Actividad04-Equipo03/tasktracker/src/test/java/cl/kibernumacademy/models/TaskTrackerTest.java
@@ -84,5 +84,37 @@ public class TaskTrackerTest {
         // Verifica la eliminación de la tarea
         tracker.deleteProduct(taskId);
         assertThat(tracker.getList(), iterableWithSize(0));
+
+
+
+public class TaskManagerTest {
+    TaskManager manager;
+
+    @BeforeEach
+    private void setUp() {
+        manager = new TaskManager();
+    }
+
+    // TOdO: assertions de JUnit y Hamcrest. assumeTrue o assumingThat. ParametrizedTest. Before/After.Each
+    @DisplayName("Prueba de agregación de tareas. (title, description, state = false)")
+    @Test 
+    void shouldAddNewUnfinishedTask() {
+        
+    }
+
+    @DisplayName("Prueba de chack de tarea completada (state = true)")
+    @Test
+    void shouldMarkATaskAsDone() {
+
+    }
+    
+    @DisplayName("Prueba de retorno de solo tareas pendientes")
+    @Test
+    void shouldShowOnlyPendingTasksAsAList(){
+
+    }
+
+}
+
     }
 }
