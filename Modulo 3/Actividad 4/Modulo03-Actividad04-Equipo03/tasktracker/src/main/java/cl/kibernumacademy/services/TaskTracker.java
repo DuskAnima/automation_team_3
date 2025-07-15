b48628119ddd106128d8302660829e32a27f672c
Ricardo <ricardo.ponce.galaz@gmail.com>
2025-07-14 23:04:46 -0400

    Actividad 4

---
+++ Modulo 3/Actividad 4/Modulo03-Actividad04-Equipo03/tasktracker/src/main/java/cl/kibernumacademy/services/TaskTracker.java
@@ -0,0 +1,53 @@
+package cl.kibernumacademy.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import cl.kibernumacademy.models.Task;
+
+public class TaskTracker {
+    private final List<Task> tasks = new ArrayList<>();
+    private int nextId = 1;
+
+    public void addTask(String title, String description, boolean state) {
+        if (title   == null || title.isBlank()) throw new IllegalArgumentException("Titulo inválido");
+        if (description == null || description.isBlank()) throw new IllegalArgumentException("Descripción inválida");
+        Task newTask = new Task(nextId++, title, description, state);
+        tasks.add(newTask);
+    }
+
+    public void updateTitle(int id, String newTitle) {
+        if (newTitle   == null || newTitle.isBlank()) throw new IllegalArgumentException("Titulo inválido");
+        Task task = getTaskById(id);
+        task.setTitle(newTitle);
+    }
+
+    public void updateDescription(int id, String newDescription) {
+        if (newDescription == null || newDescription.isBlank()) throw new IllegalArgumentException("Descripción inválida");
+        Task task = getTaskById(id);
+        task.setDescription(newDescription);
+    }
+
+    public void updateState(int id, boolean newState) {
+        Task task = getTaskById(id);
+        task.setState(newState);
+    }
+
+    public void deleteProduct(int id) {
+        Task task = getTaskById(id);
+        tasks.remove(task);
+    }
+
+    // Función privada encapsulada por ser de uso reiterativo. Previene cualquier retorno de objeto vacío
+    private Task getTaskById(int id) { 
+        return tasks.stream().
+            filter(task -> task.getId() == id).
+            findFirst().
+            orElseThrow(() -> new IllegalArgumentException("Esta tarea no existe"));
+    }
+
+    public List<Task> getList() {
+        return Collections.unmodifiableList(tasks);
+    }
+}
