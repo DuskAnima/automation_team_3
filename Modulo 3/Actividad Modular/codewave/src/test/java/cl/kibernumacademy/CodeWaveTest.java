dcbc9e64542dad13481cbf319f46a287c538e029
ROSA ESTER M. <rosita.marchant@gmail.com>
2025-07-20 20:44:49 -0400

    -Creación de Actividad
    Se crea archivos necesarios:
    _Requisitos
    _Readme (con lista de integrantes)
    _Pom (con configuración necesaria)

---
+++ Modulo 3/Actividad Modular/codewave/src/test/java/cl/kibernumacademy/CodeWaveTest.java
@@ -0,0 +1,37 @@
+package cl.kibernumacademy;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
