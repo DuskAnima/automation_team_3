0657ee86b48bd14923e8c641d5a6d363127a6f3f
Margarita1209 <margarita.levil29@gmail.com>
2025-07-29 22:11:32 -0400

    Base modulo 05 actividad01

---
+++ Modulo05/login/src/main/java/cl/kibernumacademy/login/Application.java
@@ -0,0 +1,8 @@
+package cl.kibernumacademy.login;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Modulo05/login/src/test/java/cl/kibernumacademy/login/pages/LoginPage.java
@@ -0,0 +1,5 @@
+package cl.kibernumacademy.login.pages;
+
+public class LoginPage {
+
+}
