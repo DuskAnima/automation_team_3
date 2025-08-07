60447d1d3b3946d432ecd1d8d49b3bf7e72381d8
ROSA ESTER M. <rosita.marchant@gmail.com>
2025-08-07 15:31:53 -0400

    -- se crea runcucumber

---
+++ Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/RunCucumberTest.java
@@ -0,0 +1,20 @@
+package cl.academy.transferencias;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="cl.academy.transferencias.steps, cl.academy.transferencias.hooks")
+@ConfigurationParameter(
+  key = PLUGIN_PROPERTY_NAME,
+  value = "pretty, html:target/cucumber-report.html"
+)
+public class RunCucumberTest {
+    
+}
