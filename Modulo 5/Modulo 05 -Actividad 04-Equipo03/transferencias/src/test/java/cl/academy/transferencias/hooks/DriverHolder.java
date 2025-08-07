1d38512d951a2ec1cffa5e6396303a232d515788
ROSA ESTER M. <rosita.marchant@gmail.com>
2025-08-07 14:55:03 -0400

    --se crean hooks

---
+++ Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/hooks/DriverHolder.java
@@ -0,0 +1,5 @@
+package cl.academy.transferencias.hooks;
+import org.openqa.selenium.WebDriver;
+public class DriverHolder {
+    
+}
---
+++ Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/hooks/Hooks.java
@@ -0,0 +1,17 @@
+package cl.academy.transferencias.hooks;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+    
+}
