99c1657c42eab56a2090f414f91ecd8e74b32b2c
ROSA ESTER M. <rosita.marchant@gmail.com>
2025-08-07 14:12:50 -0400

    Create LoginPage.java

---
+++ Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package cl.academy.pages;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import cl.academy.
+
+public class LoginPage {
+    
+}
