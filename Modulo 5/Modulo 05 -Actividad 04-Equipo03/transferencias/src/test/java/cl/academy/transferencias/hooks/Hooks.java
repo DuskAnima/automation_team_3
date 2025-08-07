289b1552e1019ec48f8aa29f49146d90c7c99774
ROSA ESTER M. <rosita.marchant@gmail.com>
2025-08-07 14:59:16 -0400

    -- se cambia página de prueba entre otros

--- Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/hooks/Hooks.java
+++ Modulo 5/Modulo 05 -Actividad 04-Equipo03/transferencias/src/test/java/cl/academy/transferencias/hooks/Hooks.java
@@ -13,5 +13,27 @@ import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks {
+
+    @Before
+  public void setUp() {
+    WebDriver driver = WebDriverManager.chromedriver().create();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    DriverHolder.set(driver);
+    driver.get("https://web-actividad-4.netlify.app/");
+  }
+
+
+  @After
+  public void tearDown(Scenario scenario) {
+    // Obtener el driver que se esta usando
+    WebDriver driver = DriverHolder.get();
+    if(scenario.isFailed()) {
+      byte[] shot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      scenario.attach(shot, "image/png", scenario.getName());
+    }
+    driver.quit(); 
+    DriverHolder.remove();  
+  }
     
 }
